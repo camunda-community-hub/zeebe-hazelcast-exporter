3d6e57bac6317ba4ba594902f78e1119fd4d1463
Christopher Zell <Zelldon91@googlemail.com>
2019-01-15 21:24:46 +0100

    chore(proto): fix generation

--- connector-csharp/gen-proto/gen-grpc.sh
+++ connector-csharp/gen-proto/gen-grpc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-zeebeVersion='develop'
+schemaVersion='master'
 protoFile=schema.proto
-protoFilePath=../proto/
+protoFilePath=./
 genPath=Connector/Zeebe/Hazelcast/Connector
 
 protoc=./packages/Google.Protobuf.Tools.3.6.1/tools/linux_x64/protoc
@@ -10,6 +10,10 @@ protoc=./packages/Google.Protobuf.Tools.3.6.1/tools/linux_x64/protoc
 # restore packages
 echo -e "nuget restore Connector.sln\n"
 nuget restore Connector.sln
+
+# get schame proto file
+echo -e "wget https://raw.githubusercontent.com/zeebe-io/zeebe-exporter-protobuf/${schemaVersion}/src/main/proto/${protoFile}\n"
+wget https://raw.githubusercontent.com/zeebe-io/zeebe-exporter-protobuf/${schemaVersion}/src/main/proto/${protoFile}
 
 # generate protobuf
 echo "
