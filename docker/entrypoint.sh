00d6cd774f7251aa9865edf94c8f2cfb2e014ad5
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2020-02-21 17:13:33 +0100

    chore(docker): fixed entrypoint shell script errors

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,6 +10,7 @@ EXPORTER_HOME=${EXPORTER_HOME:-"/usr/share/zeebe/${EXPORTER_ID}"}
 SHARED_DIR=${SHARED_DIR:-"/usr/share/zeebe/exporters"}
 EXPORTER_JAR=${EXPORTER_JAR:-"${EXPORTER_HOME}/${EXPORTER_ID}.jar"}
 SHARED_JAR=${SHARED_JAR:-"${SHARED_DIR}/${EXPORTER_ID}.jar"}
+CONFIG_PATH=${CONFIG_PATH:-"${SHARED_DIR}/${EXPORTER_ID}.cfg.toml"}
 
 if [[ ! -f "${EXPORTER_JAR}" ]]; then
   echo "Expected EXPORTER_JAR to point to an existing JAR, but no such file exists at ${EXPORTER_JAR}"
@@ -23,7 +24,7 @@ fi
 
 ENABLED_VALUE_TYPES=${ENABLED_VALUE_TYPES:-"JOB,WORKFLOW_INSTANCE,DEPLOYMENT,INCIDENT,TIMER,VARIABLE,MESSAGE,MESSAGE_SUBSCRIPTION,MESSAGE_START_EVENT_SUBSCRIPTION"}
 UPDATE_POSITION=${UPDATE_POSITION:-"false"}
-cat << EOF > "${SHARED_DIR}/${EXPORTER_ID}.cfg.toml"
+cat << EOF >> ${CONFIG_PATH}
 [[exporters]]
 id = "${EXPORTER_ID}"
 className = "io.zeebe.hazelcast.exporter.HazelcastExporter"
